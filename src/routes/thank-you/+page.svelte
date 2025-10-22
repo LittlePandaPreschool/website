1951afd85202093282c81d6b1316331d0210815f
Adrian <adrian.z.mei@gmail.com>
2025-10-22 03:22:55 -0700

    feat: form

---
+++ src/routes/thank-you/+page.svelte
@@ -0,0 +1,5 @@
+<main class="container mx-auto px-6 py-12 text-center">
+    <h1 class="text-4xl font-bold text-text-main mb-4">Thank You!</h1>
+    <p class="text-xl text-text-main">Your submission has been received.</p>
+    <a href="/" class="btn-primary mt-8 inline-block">Return to Home</a>
+</main>
